b383fc2792b20e579cb0b0711bcb4f4238561a4b
otuhs_d <yekcim.114@gmail.com>
2019-03-01 22:45:59 +0900

    test: リクエストSpec作成(ログイン状態再現が未対応)

--- spec/factories/achievement.rb
+++ spec/factories/achievement.rb
@@ -2,4 +2,12 @@ FactoryBot.define do
   factory :achievement do
     title { 'test_title' }
   end
+
+  factory :marriage, class: Achievement do
+    title { 'Marriage' }
+  end
+
+  factory :childbirth, class: Achievement do
+    title { 'Childbirth' }
+  end
 end
---
+++ spec/requests/achievements_controller_spec.rb
@@ -0,0 +1,76 @@
+describe AchievementsController, type: :request do
+  describe 'GET #index' do
+    before do
+      create(:marriage)
+      create(:childbirth)
+    end
+
+    it 'リクエストが成功する' do
+      get achievements_url
+      expect(response.status).to eq 200
+    end
+
+    it '実績タイトルが表示されている' do
+      get achievements_url
+      expect(response.body).to include 'Marriage'
+      expect(response.body).to include 'Childbirth'
+    end
+  end
+
+  describe 'GET #show' do
+    context '実績が存在する場合' do
+      let(:marriage) { create(:marriage) }
+
+      it 'リクエストが成功する' do
+        get achievement_url(marriage.id)
+        expect(response.status).to eq 200
+      end
+
+      it '実績タイトルが表示されている' do
+        get achievement_url(marriage.id)
+        expect(response.body).to include 'Marriage'
+      end
+    end
+
+    context '実績が存在しない場合' do
+      subject { -> { get achievement_url(1) } }
+
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
+
+  describe 'GET #new' do
+    it 'リクエストが成功する' do
+      get new_achievement_url
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'POST #create' do
+    context 'パラメータが妥当な場合' do
+      it 'リクエストが成功する' do
+        post achievements_url, params: { achievement: attributes_for(:achievement) }
+        expect(response.status).to eq 302
+      end
+
+      it '実績が登録される' do
+        expect do
+          post achievements_url, params: { achievement: attributes_for(:achievement) }
+        end.to change(Achievement, :count).by(1)
+      end
+
+      it 'リダイレクトする' do
+        post achievements_url, params: { achievement: attributes_for(:achievement) }
+        expect(response).to redirect_to Achievement.last
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it 'リクエストが成功する' do
+      end
+
+      it '実績が登録されない' do
+      end
+    end
+  end 
+end
