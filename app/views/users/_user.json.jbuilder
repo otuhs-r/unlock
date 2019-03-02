5c3d980a1a77b480c066414c85ef110ca2bad1f2
otuhs_d <yekcim.114@gmail.com>
2019-03-02 18:39:52 +0900

    reafactor: ユーザ周りのページを整理

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :provider, :uid, :nickname, :image_url, :created_at, :updated_at
-json.url user_url(user, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users
+  root 'achievements#index'
+  resources :users, only: [:show]
   resources :achievements, only: [:index, :show, :new, :create]
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout' => 'sessions#destroy'
