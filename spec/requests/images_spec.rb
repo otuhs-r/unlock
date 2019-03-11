296355010e437d40d527fdc31c549be6906ef812
otuhs_d <yekcim.114@gmail.com>
2019-03-11 22:50:58 +0900

    refactor: fix typo

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -1,12 +1,12 @@
 describe UsersController, type: :request do
   describe 'GET #ogp' do
     it 'リクエストが成功する' do
-      get iamges_ogp_url
+      get images_ogp_url
       expect(response.status).to eq 200
     end
 
     it 'ダウンロードされる' do
-      get iamges_ogp_url
+      get images_ogp_url
       expect(response.content_type).to eq('image/png')
     end
   end
