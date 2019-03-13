991bcf0fae76684d0c5306e9b7d4ce428088958c
otuhs_d <yekcim.114@gmail.com>
2019-03-13 22:29:38 +0900

    feat: 実績を解除か未解除かでわけて表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit]
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:bookmarks).find(params[:id])
+    @unlocked_bookmarks = @user.bookmarks.unlocked
+    @locked_bookmarks = @user.bookmarks.locked
   end
 
   def edit
+    @unlocked_bookmarks = current_user.bookmarks.unlocked
+    @locked_bookmarks = current_user.bookmarks.locked
   end
 
   private
