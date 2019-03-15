d0d6c658ccdace9032ee99154aab0b4de56555cd
otuhs_d <yekcim.114@gmail.com>
2019-03-15 23:42:47 +0900

    feat: タグ付けできるようにした

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,6 +2,7 @@ class Achievement < ApplicationRecord
   has_many :bookmarks
   has_many :users, through: :bookmarks
   accepts_nested_attributes_for :bookmarks
+  acts_as_taggable
 
   validates :title, presence: true, length: { maximum: 100 }, uniqueness: true
 end
