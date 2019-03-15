12ed0a9e2bfcb2afceba000f034dcf8134f1bd8a
otuhs_d <yekcim.114@gmail.com>
2019-03-15 23:43:47 +0900

    refactor: 不要メソッド削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,6 @@ class UsersController < ApplicationController
 
   private
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:provider, :uid, :nickname, :image_url)
-  end
-
   def correct_user
     user = User.find(params[:id])
     redirect_to root_path unless user == current_user
