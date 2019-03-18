45908216985c8dd95420afb99a5bba3bf99f2189
otuhs_d <yekcim.114@gmail.com>
2019-03-18 20:29:50 +0900

    test: タグの個数のテスト

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -4,35 +4,47 @@ describe Achievement do
   describe 'validation' do
     subject { achievement }
 
-    context 'all valid params' do
+    context 'すべて妥当なパラメータの場合' do
       let(:achievement) { build(:achievement) }
 
-      it 'is valid' do
+      it '有効' do
+        achievement.tag_list = '1, 2, 3, 4, 5'
+        achievement.save
         is_expected.to be_valid
       end
     end
 
-    context 'empty title' do
+    context 'タイトルが空の場合' do
       let(:achievement) { build(:achievement, title: '') }
 
-      it 'is NOT valid' do
+      it '無効' do
         is_expected.to be_invalid
       end
     end
 
-    context 'duplicated title' do
+    context 'タイトルが重複している場合' do
       let!(:duplicated_achievement) { create(:achievement) }
       let(:achievement) { build(:achievement) }
 
-      it 'is NOT valid' do
+      it '無効' do
         is_expected.to be_invalid
       end
     end
 
-    context 'too long title' do
+    context 'タイトルが長すぎる場合' do
       let(:achievement) { build(:achievement, title: 'a' * 101) }
 
-      it 'is NOT valid' do
+      it '無効' do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'タグが多すぎる場合' do
+      let(:achievement) { build(:achievement) }
+
+      it '有効' do
+        achievement.tag_list = '1, 2, 3, 4, 5, 6'
+        achievement.save
         is_expected.to be_invalid
       end
     end
