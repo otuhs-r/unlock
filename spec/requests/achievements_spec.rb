ea9c1e6f62ad974587087cb175e808e145696c1d
otuhs_d <yekcim.114@gmail.com>
2019-03-19 15:43:28 +0900

    feat: すでに存在する実績を作ろうとしたときにエラーにならないようにした

--- spec/requests/achievements_spec.rb
+++ spec/requests/achievements_spec.rb
@@ -27,38 +27,75 @@ describe AchievementsController, type: :request do
 
   describe 'POST #create' do
     context 'パラメータが妥当な場合' do
-      let(:user) { create(:user) }
-      let(:achievement) { create(:achievement) }
-      before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user) }
+      context '新しい実績が作成される場合' do
+        let(:user) { create(:user) }
+        let(:achievement) { create(:achievement) }
+        before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user) }
 
-      it 'リクエストが成功する' do
-        post achievements_url, params: { achievement: attributes_for(:achievement) }
-        expect(response.status).to eq 302
-      end
-
-      it '実績が登録される' do
-        expect do
+        it 'リクエストが成功する' do
           post achievements_url, params: { achievement: attributes_for(:achievement) }
-        end.to change(Achievement, :count).by(1)
-      end
+          expect(response.status).to eq 302
+        end
 
-      it 'ブックマークされる' do
-        expect do
-          post achievements_url, params: { achievement: attributes_for(:achievement) }
-        end.to change(Bookmark, :count).by(1)
-      end
+        it '実績が登録される' do
+          expect do
+            post achievements_url, params: { achievement: attributes_for(:achievement) }
+          end.to change(Achievement, :count).by(1)
+        end
 
-      context '登録のみの場合' do
-        it '実績一覧ページにリダイレクトする' do
-          post achievements_url, params: { achievement: attributes_for(:achievement), only_create: '' }
-          expect(response).to redirect_to achievements_url
+        it 'ブックマークされる' do
+          expect do
+            post achievements_url, params: { achievement: attributes_for(:achievement) }
+          end.to change(Bookmark, :count).by(1)
+        end
+
+        context '登録のみの場合' do
+          it 'ユーザーページにリダイレクトする' do
+            post achievements_url, params: { achievement: attributes_for(:achievement), only_create: '' }
+            expect(response).to redirect_to user_url(user)
+          end
+        end
+
+        context '登録かつ解除の場合' do
+          it '実績解除ページにリダイレクトする' do
+            post achievements_url, params: { achievement: attributes_for(:achievement) }
+            expect(response).to redirect_to user_bookmark_path(user, Bookmark.last)
+          end
         end
       end
 
-      context '登録かつ解除の場合' do
-        it '実績解除ページにリダイレクトする' do
+      context 'すでに実績が存在する場合' do
+        let(:user) { create(:user) }
+        let!(:achievement) { create(:achievement) }
+        before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user) }
+
+        it 'リクエストが成功する' do
           post achievements_url, params: { achievement: attributes_for(:achievement) }
-          expect(response).to redirect_to user_bookmark_path(user, Bookmark.last)
+          expect(response.status).to eq 302
+        end
+
+        it '実績が登録されない' do
+          expect do
+            post achievements_url, params: { achievement: attributes_for(:achievement) }
+          end.to change(Achievement, :count).by(0)
+        end
+
+        context 'ブックマーク済の場合' do
+          let!(:bookmark) { create(:bookmark, user: user, achievement: achievement) }
+
+          it 'ブックマークされない' do
+            expect do
+              post achievements_url, params: { achievement: attributes_for(:achievement) }
+            end.to change(Bookmark, :count).by(0)
+          end
+        end
+
+        context 'ブックマークしていない場合' do
+          it 'ブックマークされる' do
+            expect do
+              post achievements_url, params: { achievement: attributes_for(:achievement) }
+            end.to change(Bookmark, :count).by(1)
+          end
         end
       end
     end
