9c3244138e78137902b5c81098f57d1b0f7f6cde
otuhs_d <yekcim.114@gmail.com>
2019-03-31 12:30:57 +0900

    refactor: 実績たちのソート整理

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,6 @@ class TagsController < ApplicationController
 
   def show
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
-    @achievements = Achievement.tagged_with(@tag).page(params[:page]).per(10)
+    @achievements = Achievement.left_joins(:bookmarks).tagged_with(@tag).group(:id).order(Arel.sql('count(user_id) desc')).page(params[:page]).per(10)
   end
 end
