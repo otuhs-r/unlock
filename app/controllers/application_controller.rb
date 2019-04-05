e97ab1c26979b9e06d565522505bb3952446f94c
otuhs_d <yekcim.114@gmail.com>
2019-04-05 21:48:03 +0900

    fix: heroku の環境は常に production に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.staging?
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if ENV['BASIC_AUTH']
   helper_method :current_user
 
   def login_required
